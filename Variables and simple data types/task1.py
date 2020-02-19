259284e71e8883954df0e7f298976e9ddc017da5
Alexsik76 <alex@jurist.vn.ua>
2020-02-19 12:44:50 +0200

    finish less2 task1

---
+++ Variables and simple data types/task1.py
@@ -0,0 +1,6 @@
+# a program that has your name and the current day of the week
+# stored as separate variables and then prints a message
+name = 'Alex'
+day = 'Tuesday'
+result = 'Good day ' + name + '! ' + day + ' is a perfect day to learn some python.'
+print(result)
