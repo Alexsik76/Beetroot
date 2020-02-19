05ea77ff08e6460fc0f20570d6995d35d4ca2550
Alexsik76 <alex@jurist.vn.ua>
2020-02-19 10:03:03 +0200

    Finish task2

--- First steps/task2.py
+++ First steps/task2.py
@@ -5,4 +5,7 @@ print('Hello world!')
 # 'Hello world! Hello world! Hello world!'
 word = 'Hello world! '
 print(word*3)
+# Hello world!!!
+# Hello world!!!
+# Hello world!!!
 print(word[:-1], word[:-1], word[:-1], sep='!!\n', end='!!')
