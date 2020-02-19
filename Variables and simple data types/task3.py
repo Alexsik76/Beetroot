4ac0b9985fb2a01d279f89105a14654346b83f2a
Alexsik76 <alex@jurist.vn.ua>
2020-02-19 13:07:35 +0200

    finish less2 task3

---
+++ Variables and simple data types/task3.py
@@ -0,0 +1,19 @@
+# Program with 2 numbers saved in separate variables a and b,
+# then print the results of some arithmetic operations
+a = 5
+b = 2
+
+# Addition
+print('Addition = ' + str(a + b))
+# Subtraction
+print('Subtraction = ' + str(a - b))
+# Division
+print('Division = ' + str(a / b))
+# Multiplication
+print('Multiplication = ' + str(a * b))
+# Exponent (Power)
+print('Exponent (Power) = ' + str(a ** b))
+# Modulus
+print('Modulus = ' + str(a % b))
+# Floor division
+print('Floor division = ' + str(a // b))
