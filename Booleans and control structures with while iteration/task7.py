ac443b40cb060c74492172bac9deec8aed1f3476
Alexsik76 <alex@jurist.vn.ua>
2020-02-22 18:49:23 +0200

    FizzBizz

--- Booleans and control structures with while iteration/task7.py
+++ Booleans and control structures with while iteration/task7.py
@@ -1,7 +1,13 @@
 import random
 curent_temp = random.randint(-25, 35)
 print('T = ' + str(curent_temp))
-if curent_temp > 24:
+if curent_temp > 27:
+    print('Я иду на пляж')
+elif 18 < curent_temp < 28:
     print('Я иду гулять')
+elif 10 < curent_temp < 19:
+    print('Я иду гулять в шапке')
+elif 0 < curent_temp < 11:
+    print('Я иду гулять в пальто и шапке')
 else:
     print('Я сижу дома')
