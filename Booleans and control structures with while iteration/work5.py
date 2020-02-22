e5c7d172b1669283625c7b6ea92eae690c61e234
Alexsik76 <alex@jurist.vn.ua>
2020-02-22 17:56:25 +0200

    11

--- Booleans and control structures with while iteration/work5.py
+++ Booleans and control structures with while iteration/work5.py
@@ -1,5 +1,6 @@
 text = 'Hello world'
 print('Колличество символов в строке: ' + str(len(text)))
+print('Колличество букв в строке: ' + str(len(text) - text.count(' ')))
 print('В верхнем регистре: ' + text.upper())
 print('В нижнем регистре: ' + text.lower())
 text_upper = text.upper()
