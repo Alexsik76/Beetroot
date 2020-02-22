f529d02f1d3026634d78d3c69006db062771e1c7
Alexsik76 <alex@jurist.vn.ua>
2020-02-22 17:33:19 +0200

    text function

---
+++ Booleans and control structures with while iteration/work5.py
@@ -0,0 +1,6 @@
+text = 'Hello world'
+print('Колличество символов в строке: ' + str(len(text)))
+print('В верхнем регистре: ' + text.upper())
+print('В нижнем регистре: ' + text.lower())
+text_upper = text.upper()
+print('Текст с заглавными: ' + text_upper.capitalize())
