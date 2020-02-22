57e0df22cfab0c2aae482caf08d16e74be1813b2
Alexsik76 <alex@jurist.vn.ua>
2020-02-22 13:17:31 +0200

    l3t2

---
+++ Booleans and control structures with while iteration/task2.py
@@ -0,0 +1,7 @@
+phone_num = input('Введите номер телефона: ')
+if phone_num.isnumeric() and len(phone_num) == 10:
+    print('Спасибо за корректный номер')
+elif not phone_num.isnumeric():
+    print('Номер содержит не только цифры')
+elif not len(phone_num) == 10:
+    print('Длина номера отличается от 10')
