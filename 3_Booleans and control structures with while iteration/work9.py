5e7ffa2171da1bb7aaaf70cef58b43a52cfe081d
Alexsik76 <alex@jurist.vn.ua>
2020-02-24 09:43:06 +0200

    add l4 task1

--- 3_Booleans and control structures with while iteration/work9.py
+++ 3_Booleans and control structures with while iteration/work9.py
@@ -1,9 +1,9 @@
 a, b = 1, 1
 fib_sum = 0
-while b < 40:
+while b < 4_000_000_000:
     if b % 2 == 0:
         fib_sum += b
     a, b = b, a + b
-print(a)
-print(b)
+# print(a)
+# print(b)
 print(fib_sum)
---
+++ 4_Input and blitz introduction to imports/task1.py
@@ -0,0 +1,11 @@
+import random
+secret_num = random.randint(1, 10)
+while True:
+    user_num = int(input('Введите целое число от 1 до 10: '))
+    if user_num == secret_num:
+        print('Вы угадали')
+        break
+    elif user_num < secret_num:
+        print('Пробуйте больше!')
+    else:
+        print('Пробуйте меньше!')
