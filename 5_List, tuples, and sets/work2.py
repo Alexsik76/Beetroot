43b1aac51e030b83d99d84713f21b54877fee527
Alexsik76 <alex@jurist.vn.ua>
2020-02-26 19:41:41 +0200

    work2

---
+++ 5_List, tuples, and sets/work2.py
@@ -0,0 +1,20 @@
+from random import randint
+list1 = []
+list2 = []
+i = 0
+while i < 10:
+    a = randint(1, 10)
+    b = randint(1, 10)
+    list1.append(a)
+    list2.append(b)
+    i += 1
+for j, v in enumerate(list1):
+    print(j, v)
+j = 0
+for j, v in enumerate(list2):
+    print(j, v)
+j = 0
+list3 = list1
+list3.extend(list2)
+for j, v in enumerate(list3):
+    print(j, v)
