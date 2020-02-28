beedfd3f015807c1461dc45a348e0cd7e548cd53
Alexsik76 <alex@jurist.vn.ua>
2020-02-28 14:21:37 +0200

    added comments

--- 5_List, tuples, and sets/work2_1.py
+++ 5_List, tuples, and sets/work2_1.py
@@ -1,3 +1,4 @@
+# generating 2 lists with random items
 from random import randint
 list1 = []
 list2 = []
@@ -8,8 +9,11 @@ while i < 10:
     list1.append(a)
     list2.append(b)
     i += 1
+# generating list with indexes
 list_n = range(10)
+# combinating every item from each list with an index
 list1_fin = list(map(str, zip(list_n, list1)))
 list2_fin = list(map(str, zip(list_n, list2)))
+# printing each list
 print('\n'.join(list1_fin))
 print('\n'.join(list2_fin))
