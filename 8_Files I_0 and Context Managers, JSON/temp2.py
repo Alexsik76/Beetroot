7845643b4c9e46ecec50c48ef319fd5cf5bd2acc
Alexsik76 <alex@jurist.vn.ua>
2020-03-12 12:26:13 +0200

    readline experiment

--- 8_Files I_0 and Context Managers, JSON/temp2.py
+++ 8_Files I_0 and Context Managers, JSON/temp2.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     import pyreadline as readline
 s = 'abcdesf'
+print('Бажаєте змінити прізвище: ')
 readline.set_startup_hook(lambda: readline.insert_text(s))
-input('Бажаєте змінити прізвище: ')
-print(s)
 s = input('')
+print(s)
