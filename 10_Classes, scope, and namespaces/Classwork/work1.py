987fd1933c6e086c175381fda249e87b630bfdb5
Alexsik76 <alex@jurist.vn.ua>
2020-03-14 18:56:47 +0200

    go_home

--- 10_Classes, scope, and namespaces/Classwork/work1.py
+++ 10_Classes, scope, and namespaces/Classwork/work1.py
@@ -1,15 +1,24 @@
 class Person:
     number_on_lesson = 0
     list_on_lesson = []
+    all_have_money = 0
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.__have_money = 10
 
     def go_to_lesson(self):
         print(f'{self.name} пішов на лекцію.')
         Person.number_on_lesson += 1
         Person.list_on_lesson.append(self.name)
+        Person.all_have_money += self.__have_money
+
+    def go_home(self):
+        print(f'{self.name} пішов додому.')
+        Person.number_on_lesson -= 1
+        Person.list_on_lesson.remove(self.name)
+        Person.all_have_money -= self.__have_money
 
     def how_much():
         print(f'Всього на лекції {Person.number_on_lesson}')
@@ -20,6 +29,9 @@ class Person:
             print(f'{item}')
         Person.how_much()
 
+    def display_money():
+        print(f'У присутніх на лекції всього {Person.all_have_money} грошей')
+
 
 persons = []
 persons.append(Person('Vova', 13))
@@ -27,6 +39,11 @@ persons.append(Person('Julia', 22))
 persons.append(Person('Serg', 25))
 persons[1].go_to_lesson()
 persons[2].go_to_lesson()
+Person.display_money()
 Person.who_on_lesson()
 Person.go_to_lesson(persons[0])
 Person.who_on_lesson()
+Person.display_money()
+persons[0].go_home()
+Person.who_on_lesson()
+Person.display_money()
