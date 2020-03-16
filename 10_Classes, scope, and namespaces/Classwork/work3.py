f6037cfea448697c44ab13819bd2f47709058ee7
Alexsik76 <alex@jurist.vn.ua>
2020-03-16 20:56:05 +0200

    glob

---
+++ 10_Classes, scope, and namespaces/Classwork/work3.py
@@ -0,0 +1,8 @@
+# from pathlib import Path
+import glob
+# print(glob.glob('*.*'))
+# path1 = Path(r"C:\Drivers")
+# print(glob.glob(r'path1\*.*'))
+# print(glob.glob('../../../../../*.*'))
+a = glob.glob('c:/Drivers/*')
+print(a)
