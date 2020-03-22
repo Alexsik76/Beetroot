6606175696eccff237adf60cddcc6a0598239d77
Alexsik76 <alex@jurist.vn.ua>
2020-03-22 19:21:39 +0200

    1

--- 10_Classes, scope, and namespaces/Classwork/temp.py
+++
@@ -1,7 +0,0 @@
-import json #подключаем библиотеку для работы с json
-
-stroke = '{"food": "banana"}' #json-строка
-
-stroke_s = json.loads(stroke) #загоняем в переменную результат чтения json-строки
-
-print(stroke_s) #выводим результат на экран
--- 10_Classes, scope, and namespaces/temp1.py
+++
@@ -1,3 +0,0 @@
-list1 = ["BBC", "Discovery", "TV1000"]
-dict1 = dict(zip(range(1, len(list1) + 1), list1))
-print(dict1, type(dict1))
