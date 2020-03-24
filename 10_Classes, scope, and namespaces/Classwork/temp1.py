72c3485487543abdfe29376057edfc2286fcb098
Alexsik76 <alex@jurist.vn.ua>
2020-03-24 19:49:19 +0200

    the best time

--- 10_Classes, scope, and namespaces/Classwork/temp1.py
+++ 10_Classes, scope, and namespaces/Classwork/temp1.py
@@ -3,12 +3,24 @@ start = input('Введіть час початку: ')
 finish = input('Введіть час закінчення: ')
 t4 = datetime.strptime(start, '%H:%M')
 t5 = datetime.strptime(finish, '%H:%M')
-print(t4)
-print(t5)
-t1 = t5 - t4
-print(str(t1))
-# x = t1.total_seconds()
-x = datetime.strptime(str(t1), '%H:%M:%S')
-h = x.hour
-m = x.minute
-print(f'Послуга надавалась {x.hour} годин(и), {x.minute} хвилин(и)')
+delta = t5 - t4
+
+
+def last_letter(number):
+    last_number = int(str(number)[-1])
+    if last_number == 1:
+        last_letter = 'у'
+    elif 1 < last_number < 5:
+        last_letter = 'и'
+    else:
+        last_letter = ''
+    return last_letter
+
+
+x = datetime.strptime(str(delta), '%H:%M:%S')
+hll = last_letter(x.hour)
+mll = last_letter(x.minute)
+first_text = 'надавалась ' if (x.hour > 0 or x.minute > 0) else 'не надавалась'
+hour_text = f'{str(x.hour)} годин{hll}, ' if x.hour > 0 else ''
+min_text = f'{str(x.minute)} хвилин{mll}.' if x.minute > 0 else '.'
+print(f'Послуга {first_text}{hour_text}{min_text}')
