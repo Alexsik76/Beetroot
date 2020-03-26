aadc7ac2fb187f4f4d39e6e65f58367ea411ef0b
Alexsik76 <alex@jurist.vn.ua>
2020-03-26 15:19:59 +0200

    333

--- 11_Inheritance/Classwork/work1_1.py
+++ 11_Inheritance/Classwork/work1_1.py
@@ -12,23 +12,13 @@ class Bikes(Vehicles):
 
 
 class Pedal_bikes(Bikes):
-    def __init__(self, *args):
+    def __init__(self):
         super().__init__()
         self.power = 0.5
         self.speed = 15
 
 
-class Motor_bikes(Bikes):
-    def __init__(self):
-        self.power = 60
-        self.speed = 120
-
-
-v0 = Vehicles()
-print(v0.wheels, v0.speed, v0.power)
-b0 = Bikes()
-print(b0.wheels, b0.speed, b0.power)
-pb = Pedal_bikes(3, 30, 35)
+pb = Pedal_bikes()
 print(pb.wheels, pb.speed, pb.power)
 # b1 = Pedal_bikes()
 # print(b1.power)
