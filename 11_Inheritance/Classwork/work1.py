f13d8e674d0951bfee94292473af8142615892e1
Alexsik76 <alex@jurist.vn.ua>
2020-03-28 02:00:32 +0200

    2

--- 11_Inheritance/Classwork/work1.py
+++ 11_Inheritance/Classwork/work1.py
@@ -47,36 +47,34 @@ class MotorBikes(Bikes, Cars):
 
 
 class PickUps(Cars):
-    def __init__(self):
-        super().__init__()
-        self.power = 60
-        self.speed = 80
+    def __init__(self, wheels, speed, power):
+        super().__init__(wheels, speed, power)
 
 
 class SportCars(Cars):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, wheels, speed, power):
+        super().__init__(wheels, speed, power)
         self.power = 500
         self.speed = 300
 
 
 class EstateCars(Cars):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, wheels, speed, power):
+        super().__init__(wheels, speed, power)
         self.power = 200
         self.speed = 220
 
 
 class MediumTrucks(Trucks):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, wheels, speed, power):
+        super().__init__(wheels, speed, power)
         self.power = 600
         self.speed = 100
 
 
 class HeavyTrucks(Trucks):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, wheels, speed, power):
+        super().__init__(wheels, speed, power)
         self.power = 800
         self.speed = 120
 
