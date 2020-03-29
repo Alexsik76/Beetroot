2ca5f6b9364562af0ebcd9bf6ecfec1d93140101
Alexsik76 <alex@jurist.vn.ua>
2020-03-29 15:23:03 +0300

    15:22

---
+++ 11_Inheritance/Classwork/unicode_exp.py
@@ -0,0 +1,10 @@
+from time import sleep
+s = str(b'\xF0\x9F\x9A\xB2', 'utf-8')
+for i in range(50):
+    print(' ', end='')
+    print(s, end='')
+    sleep(0.2)
+    print('\b\b', end='')
+# s = str(b'\xF0\x9F\x8E\xB2', 'utf-8')
+# print(s, end='')
+
