fa9b899b0a361f73b960c9f7cd56d0e703dd5bac
Alexsik76 <alex@jurist.vn.ua>
2020-04-07 00:32:12 +0300

    00:32

--- 11_Inheritance/Classwork/Vehicles2/vehicles2/temp1.py
+++
@@ -1,6 +0,0 @@
-matrix = [['name', 'wheels', 'speed', 'power', 'load_capacity', 'capacity_of_people', 'fuel_costs']]
-print(matrix)
-print(type(matrix))
-matrix2 = ['name,wheels,speed,power,load_capacity,capacity_of_people,fuel_costs'.split(',')]
-print(matrix2)
-print(type(matrix2))
