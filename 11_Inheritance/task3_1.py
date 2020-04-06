84a8529371aa8a5405968b8e3195a5a26ce106e7
Alexsik76 <alex@jurist.vn.ua>
2020-04-06 19:06:20 +0300

    19:06

--- 11_Inheritance/task3_1.py
+++ 11_Inheritance/task3_1.py
@@ -33,16 +33,29 @@ class ProductStore:
     def add(self, product, amount):
         """Adds a specified quantity of a single product with a predefined price premium for your store(30 percent)"""
         self.store.append({
-            'type':     product.type,
-            'name':     product.name,
-            'price':    product.price + (product.price * 0.3),
-            'amount':   amount
+            'type': product.type,
+            'name': product.name,
+            'price': product.price + (product.price * 0.3),
+            'amount': amount
         })
 
+    @except_decorator
+    def set_discount(self, identifier, percent, identifier_type='name'):
+        """Adds a discount for all products specified by input identifiers (type or name).
+        The discount must be specified in percentage"""
+        for item in self.store:
+            if item[identifier_type] == identifier:
+                item['price'] = round((item['price'] - item['price'] * percent * 0.01), 2)
+
+    # @except_decorator
+    # def sell_product(self, product_name, amount):
 
 silmag = ProductStore()
 
 print(apples)
 silmag.add(apples, 100)
 
+print(silmag.store)
+
+silmag.set_discount('apples', 75)
 print(silmag.store)
