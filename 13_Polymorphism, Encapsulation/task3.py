52edf0d20ea7a9c1993b1a2839a362b6cb538423
Alexsik76 <alex@jurist.vn.ua>
2020-04-18 19:10:22 +0300

    finish  13/3

--- 13_Polymorphism, Encapsulation/task3.py
+++ 13_Polymorphism, Encapsulation/task3.py
@@ -7,21 +7,27 @@ class Fraction:
         assert denominator != 0, 'denominator mast bee not 0'
         self.denominator = denominator
 
+
+
     def __add__(self, other):
+        assert isinstance(other, Fraction), 'The second term of the expression must be an Fraction.object'
         rez_numerator = self.numerator * other.denominator + self.denominator * other.numerator
         rez_denominator = self.denominator * other.denominator
         return reducing(Fraction(rez_numerator, rez_denominator))
 
     def __sub__(self, other):
+        assert isinstance(other, Fraction), 'The second term of the expression must be an Fraction.object'
         other.numerator = -other.numerator
         return self.__add__(other)
 
     def __mul__(self, other):
+        assert isinstance(other, Fraction), 'The second term of the expression must be an Fraction.object'
         rez_numerator = self.numerator * other.numerator
         rez_denominator = self.denominator * other.denominator
         return reducing(Fraction(rez_numerator, rez_denominator))
 
     def __truediv__(self, other):
+        assert isinstance(other, Fraction), 'The second term of the expression must be an Fraction.object'
         if other.numerator != 0:
             other.numerator, other.denominator = other.denominator, other.numerator
             return self.__mul__(other)
@@ -58,5 +64,5 @@ z = x - y
 print(z)
 x1 = Fraction(-25, 5)
 y1 = Fraction(-1, 32)
-z1 = x1 + y1
+z1 = x1 / y1
 print(z1)
