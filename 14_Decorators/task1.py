d066b97e1c0950d457acc7390c30406614f2aa7d
Alexsik76 <alex@jurist.vn.ua>
2020-04-18 20:15:05 +0300

    finish  14/1

--- 14_Decorators/task1.py
+++ 14_Decorators/task1.py
@@ -2,7 +2,10 @@ def logger(func):
     def wrapper(*args):
         print(f'{func.__name__} called with {args[0]}', *args[1:], sep=', ')
         return func(*args)
+
     return wrapper
+
+
 @logger
 def add(x, y):
     return x + y
@@ -11,6 +14,7 @@ def add(x, y):
 @logger
 def square_all(*args):
     return [arg ** 2 for arg in args]
+
 
 add(1, 2)
 square_all(1, 2, 3)
