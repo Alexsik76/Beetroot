7a1771d3c81819e4fe14dad81fc9dfec5f2e95c5
Alexsik76 <alex@jurist.vn.ua>
2020-04-18 18:49:43 +0300

    finish  13/3

---
+++ 13_Polymorphism, Encapsulation/task3.py
@@ -0,0 +1,62 @@
+# Fraction
+
+
+class Fraction:
+    def __init__(self, numerator: int, denominator: int = 1):
+        self.numerator = numerator
+        assert denominator != 0, 'denominator mast bee not 0'
+        self.denominator = denominator
+
+    def __add__(self, other):
+        rez_numerator = self.numerator * other.denominator + self.denominator * other.numerator
+        rez_denominator = self.denominator * other.denominator
+        return reducing(Fraction(rez_numerator, rez_denominator))
+
+    def __sub__(self, other):
+        other.numerator = -other.numerator
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        rez_numerator = self.numerator * other.numerator
+        rez_denominator = self.denominator * other.denominator
+        return reducing(Fraction(rez_numerator, rez_denominator))
+
+    def __truediv__(self, other):
+        if other.numerator != 0:
+            other.numerator, other.denominator = other.denominator, other.numerator
+            return self.__mul__(other)
+        else:
+            print('Failed to divide by 0')
+
+    def __str__(self):
+        if self.denominator == 1:
+            return f'{self.numerator}'
+        elif self.numerator == 0:
+            return '0'
+        else:
+            return f'{self.numerator} / {self.denominator}'
+
+
+def reducing(obj: Fraction):
+    a, b = obj.numerator, obj.denominator
+    if b < 0:
+        a, b, = -a, -b
+    if a == 0:
+        b = 1
+    else:
+        max_num = abs(a) if abs(a) > abs(b) else abs(b)
+        for i in range(2, max_num + 1):
+            while a % i == 0 and b % i == 0:
+                a, b, = int(a / i), int(b / i)
+    obj.numerator, obj.denominator = a, b
+    return obj
+
+
+x = Fraction(125, 15)
+y = Fraction(5, 25)
+z = x - y
+print(z)
+x1 = Fraction(-25, 5)
+y1 = Fraction(-1, 32)
+z1 = x1 + y1
+print(z1)
