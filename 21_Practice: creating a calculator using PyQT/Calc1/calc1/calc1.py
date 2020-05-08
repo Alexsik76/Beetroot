956e024ade60718a89ef898706c59aef5c5ea1de
Alexsik76 <alex@jurist.vn.ua>
2020-05-08 19:47:19 +0300

    calc1_1

--- 21_Practice: creating a calculator using PyQT/Calc1/calc1/calc1.py
+++ 21_Practice: creating a calculator using PyQT/Calc1/calc1/calc1.py
@@ -1,6 +1,7 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+import operator
 from calc1design import  Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -10,3 +11,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
  # Setup numbers.
         for n in range(0, 10):
             getattr(self, 'pushButton_n%s' % n).pressed.connect(lambda v=n: self.input_number(v))
+
+        # self.reset()
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    app.setApplicationName("Calc")
+
+    window = MainWindow()
+    app.exec_()
