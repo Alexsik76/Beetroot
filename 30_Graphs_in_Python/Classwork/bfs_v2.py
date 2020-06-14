12989b1897fc48067898515c06893abfc0d1cd30
Alexsik76 <alex@jurist.vn.ua>
2020-06-14 23:37:44 +0300

    pycodestyle

--- 30_Graphs_in_Python/Classwork/bfs_v2.py
+++ 30_Graphs_in_Python/Classwork/bfs_v2.py
@@ -1,4 +1,6 @@
 from collections import deque
+
+
 # finds shortest path between 2 nodes of a graph using BFS
 def bfs_shortest_path(graph, start, goal):
     # keep track of explored nodes
@@ -9,7 +11,6 @@ def bfs_shortest_path(graph, start, goal):
     # return path if start is goal
     if start == goal:
         return "That was easy! Start = goal"
- 
     # keeps looping until all possible paths have been checked
     while queue:
         # pop the first path from the queue
@@ -28,17 +29,17 @@ def bfs_shortest_path(graph, start, goal):
                 # return path if neighbour is goal
                 if neighbour == goal:
                     founded_pathes.append(new_path)
- 
             # mark node as explored
             explored.add(node)
     if founded_pathes:
-        return min(founded_pathes, key = len)
+        return min(founded_pathes, key=len)
     else:
         # in case there's no path between the 2 nodes
         return "So sorry, but a connecting path doesn't exist :("
- 
+
+
 # sample graph implemented as a dictionary
-graph ={
+graph = {
     'Math-151': ['CS-220'],
     'CS-220': ['CS-360', 'CS-477', 'CS-151'],
     'CS-360': ['CS-490', 'CS-466'],
@@ -46,8 +47,8 @@ graph ={
     'CS-150': ['CS-220', 'CS-151', 'CS-250'],
     'CS-151': ['CS-250', 'CS-230', 'CS-360'],
     'CS-230': ['CS-466'],
-    'CS-490': ['CS-230'], 
+    'CS-490': ['CS-230'],
     'CS-466': ['CS-360']
 }
-path = bfs_shortest_path(graph, 'Math-151', 'CS-230') 
+path = bfs_shortest_path(graph, 'Math-151', 'CS-230')
 print(path)
