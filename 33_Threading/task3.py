4803cfca4b4369962d00a3d3e6b8d1da7ed9601b
Alexsik76 <alex@jurist.vn.ua>
2020-06-19 17:10:58 +0300

    1
    1

--- 33_Threading/task3.py
+++ 33_Threading/task3.py
@@ -11,11 +11,11 @@ authors = ["Yurishirox", "Zeewitje", "Magic-Prankster"]
 
 
 def read_from_API(url: str, param: str):
-    reqest = requests.get(f'{url}{param}')
-    thead = {'thead': threading.current_thread().getName()}
-    data_to_add = reqest.json()['data']
+    request = requests.get(f'{url}{param}')
+    thread = {'thread': threading.current_thread().getName()}
+    data_to_add = request.json()['data']
     for item in data_to_add:
-        item.update(thead)
+        item.update(thread)
     return data_to_add
 
 
@@ -30,21 +30,20 @@ for thread in threads:
     comments.append(thread)
     print(type(thread))
     print(dir(thread))
-    print(thread[0])
+    # print(thread[0])
 
 print(*comments)
 
 
-
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import as_completed
-values = [2,3,4,5]
-def square(n):
-   return n * n
-def main():
-   with ThreadPoolExecutor(max_workers = 3) as executor:
-      results = executor.map(square, values)
-for result in results:
-      print(result)
-if __name__ == '__main__':
-   main()
+# from concurrent.futures import ThreadPoolExecutor
+# from concurrent.futures import as_completed
+# values = [2,3,4,5]
+# def square(n):
+#    return n * n
+# def main():
+#    with ThreadPoolExecutor(max_workers = 3) as executor:
+#       results = executor.map(square, values)
+# for result in results:
+#       print(result)
+# if __name__ == '__main__':
+#    main()
