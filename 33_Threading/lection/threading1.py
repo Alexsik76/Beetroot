9aa63157d2ab2204132e134cdefdb761fa106947
Alexsik76 <alex@jurist.vn.ua>
2020-06-20 13:54:06 +0300

    thr2

--- 33_Threading/lection/threading1.py
+++ 33_Threading/lection/threading1.py
@@ -1,15 +1,14 @@
 # threading1
 import threading
 import time
-import random
 
 
 def worker(number):
-    thread_name = threading.current_thread().getName()
+    thread_name = threading.current_thread().name
     for i in range(1000):
         number = i
         time.sleep(0.001)
-    print(f"I am Worker {thread_name}, I my number is {number}")
+    print(f"I am Worker {thread_name}, my number is {number}\n")
 
 
 number = 0
@@ -18,6 +17,6 @@ t2 = threading.Thread(target=worker, name='t2', args=(number,))
 
 t1.start()
 t2.start()
-t1.join()
-t2.join()
-print('Main thread is finished')
+# t1.join()
+# t2.join()
+print('Main thread is finished', '\n')
