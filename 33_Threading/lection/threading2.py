d137dc6cc0369c858a7eca6d5a9f9ef376a9102e
Alexsik76 <alex@jurist.vn.ua>
2020-06-20 22:15:09 +0300

    5

---
+++ 33_Threading/lection/threading2.py
@@ -0,0 +1,30 @@
+import threading
+
+
+def counter(number):
+    for i in range(number):
+        x = i
+    print(x)
+
+
+def threds_task(number):
+    number = int(number / 2)
+    t1 = threading.Thread(target=counter, args=(number,))
+    t2 = threading.Thread(target=counter, args=(number,))
+
+    # start threads
+    t1.start()
+    t2.start()
+
+    # wait until threads finish their job
+    t1.join()
+    t2.join()
+
+
+def single_task(number):
+    counter(number)
+
+
+number = 10_000_000
+# threds_task(number)
+single_task(number)
